b9e7055402c58ff9843c65d7d03decaed629cb77
frontMan <mr.cdrmn277@gmail.com>
2024-02-21 18:24:05 +0300

    categories and callback form have been done

--- jQuery/tech.js
+++ jQuery/tech.js
@@ -5,22 +5,22 @@ $(function () {
     $("a.card").mouseleave(function () {
         $(this).css({"animation":"hoverDown 0.2s ease", "animation-fill-mode":"forwards"})
     })
-})
-$(window).scroll(function () {
-    var jqoffset = $(".third").offset().top
-    var scssoffset = $("footer").offset().top
+    $(window).scroll(function () {
+        var jqoffset = $(".third").offset().top
+        var scssoffset = $("footer").offset().top
 
-    if ($(window).scrollTop() + $(window).height() > jqoffset) {
-        $("body").addClass("jq-scrollbar");
-    } else {
-        $("body").removeClass("jq-scrollbar")
-    } if ($(window).scrollTop() + $(window).height() > scssoffset) {
-        $("body").addClass("scss-scrollbar");
-    } else {
-        $("body").removeClass("scss-scrollbar");
-    }
-})
-$(".lazyLoad").Lazy({
-    scrollDirection: "vertical",
-    effect: "show"
+        if ($(window).scrollTop() + $(window).height() > jqoffset) {
+            $("body").addClass("jq-scrollbar");
+        } else {
+            $("body").removeClass("jq-scrollbar")
+        } if ($(window).scrollTop() + $(window).height() > scssoffset) {
+            $("body").addClass("scss-scrollbar");
+        } else {
+            $("body").removeClass("scss-scrollbar");
+        }
+    })
+    $(".lazyLoad").Lazy({
+        scrollDirection: "vertical",
+        effect: "show"
+    })
 })
